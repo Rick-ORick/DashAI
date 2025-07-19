d8e225519cc1ea23cb7f98698364331cbe13f273
Rick-ORick <chsilvaoliveira@outlook.com>
2025-07-19 18:55:42 -0300

    Add files via upload

---
+++ babel.congif.cjs
@@ -0,0 +1,14 @@
+module.exports = {
+  presets: [
+    "@babel/preset-env",  // For compiling modern JavaScript
+    "@babel/preset-react"  // For compiling JSX
+  ],
+  plugins: [
+    "@babel/plugin-transform-numeric-separator",
+    "@babel/plugin-transform-optional-chaining",
+    "@babel/plugin-transform-nullish-coalescing-operator",
+    "@babel/plugin-transform-private-methods",
+    "@babel/plugin-transform-class-properties",
+    "@babel/plugin-transform-private-property-in-object"
+  ]
+};
