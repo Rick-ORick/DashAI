df1ceb1cb8e8f8d7a7a8519c9e60d1090eed948e
Rick-ORick <chsilvaoliveira@outlook.com>
2025-07-20 18:38:36 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
   build: {
     rollupOptions: {
-      input: 'index.html',  // Ensure this is correct
-    }
+      input: 'index.html', // Ensure Vite knows about the correct entry point
+    },
   },
-})
+});
