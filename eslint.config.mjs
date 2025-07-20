199296d1884245cb582913b0265569fb23ca0717
Rick-ORick <chsilvaoliveira@outlook.com>
2025-07-20 12:36:16 -0300

    Update eslint.config.mjs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,28 +1,28 @@
-import pluginReact from 'eslint-plugin-react';
-import globals from 'globals';
+//import pluginReact from 'eslint-plugin-react';
+//import globals from 'globals';
 
-export default {
-  files: ["**/*.{js,jsx,mjs,cjs}"],
-  languageOptions: {
-    parserOptions: {
-      ecmaVersion: 2020,
-      sourceType: 'module',
-      ecmaFeatures: {
-        jsx: true,  // Enable JSX parsing
-      }
-    },
-    globals: {
-      ...globals.browser,
-      ...globals.node,
-    },
-  },
-  plugins: {
-    react: pluginReact,
-  },
-  rules: {
-    'react/jsx-uses-react': 'off',
-    'react/react-in-jsx-scope': 'off',
-    'no-console': 'warn',
-    'no-unused-vars': 'warn',
-  },
-};
+//export default {
+  //files: ["**/*.{js,jsx,mjs,cjs}"],
+  //languageOptions: {
+    //parserOptions: {
+      //ecmaVersion: 2020,
+      //sourceType: 'module',
+      //ecmaFeatures: {
+     //   jsx: true,  // Enable JSX parsing
+    //  }
+   // },
+    //globals: {
+     // ...globals.browser,
+    //  ...globals.node,
+   // },
+  //},
+ // plugins: {
+   // react: pluginReact,
+  //},
+  //rules: {
+    //'react/jsx-uses-react': 'off',
+    //'react/react-in-jsx-scope': 'off',
+    //'no-console': 'warn',
+  //  'no-unused-vars': 'warn',
+//  },
+//};
