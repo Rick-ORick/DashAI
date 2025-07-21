36dd5ba90ba792d725ef366111aa60bd012e42fe
Rick-ORick <chsilvaoliveira@outlook.com>
2025-07-20 22:34:57 -0300

    Update and rename main.js to main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App'; // Import App.js from the src folder
+import './index.css'; // Global styles
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+// Get the root element where React will render the app
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <Router>
+      <Routes>
+        <Route path="/" element={<App />} />
+        {/* Add other routes if needed */}
+      </Routes>
+    </Router>
+  </React.StrictMode>
+);
