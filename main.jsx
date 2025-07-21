a21307f2ca497c6d932d168a2a82f53bc0b13727
Rick-ORick <chsilvaoliveira@outlook.com>
2025-07-20 23:55:29 -0300

    Update main.jsx

--- main.jsx
+++ main.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App'; // Import App.js from the src folder
+import App from './App.jsx'; // Import App.js from the src folder
 import './index.css'; // Global styles
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
