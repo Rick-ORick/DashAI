91e57c8da4de0f9d07dc484030f7b85a47e715a2
Rick-ORick <chsilvaoliveira@outlook.com>
2025-07-21 18:53:04 -0300

    Update Getting_Started.jsx

--- src/Getting_Started.jsx
+++ src/Getting_Started.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect, useRef } from "react";
-import "./Getting_Started.css";
+import "./src/Getting_Started.css";
 import "@fontsource/montserrat/700.css";
 import { Link } from "react-router-dom";
 
