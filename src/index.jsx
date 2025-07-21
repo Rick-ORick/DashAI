482a2cc831312fb3b9995f98c40a5276df92b455
Rick-ORick <chsilvaoliveira@outlook.com>
2025-07-21 20:12:44 -0300

    Update index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './src/App';
+import App from './App';
 import GettingStarted from './Getting_Started.jsx';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
