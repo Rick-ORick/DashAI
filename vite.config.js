bb390ff41be228a13507c9a04037db0feb795145
Rick-ORick <chsilvaoliveira@outlook.com>
2025-07-21 20:17:34 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,11 +4,6 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   build: {
-    rollupOptions: {
-      input: 'index.html', // Ensure this is correct
-    },
-  },
-  esbuild: {
-    jsx: 'react', // This enables JSX parsing for .js files
+    outDir: 'dist', // Must match vercel.json
   },
 });
